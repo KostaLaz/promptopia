14f01ef9e337835384d193bca7c16a509051935b
KostaLaz <kosta.lazarevski89@gmail.com>
2024-07-11 03:50:21 +0200

    fix

--- utils/db.js
+++ utils/db.js
@@ -1,22 +1,22 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 let isConnected = false;
 
-export const connectToDb = async () => {
-  mongoose.set("strictQuery", true);
-  if (isConnected) {
-    console.log("MongoDB is already connected");
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+  if(isConnected) {
+    console.log('MongoDB is already connected');
     return;
   }
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "share-prompt",
-      useNewUrlParse: true,
-      UseUnifiedTopology: true,
-    });
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     isConnected = true;
-    console.log("MongoDB coneccted");
+    console.log('MongoDB connected')
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
-};
+}
